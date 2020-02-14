645af4582b2e0a7a89631bd46eb193adf05b1267
Guillaume Gauvrit <guillaume@gauvr.it>
2020-02-14 13:29:29 +0100

    Add a first test

--- tests/functionals/features/environment.py
+++ tests/functionals/features/environment.py
@@ -1,13 +1,35 @@
 import os
 import pathlib
+from shutil import copy2
 import subprocess
 
 from behave import *
 
 
+def run_command(context):
+    def run_command_impl(command):
+        return subprocess.run(
+            command.split(),
+            capture_output=True,
+            text=True,
+        )
+    return run_command_impl
+
+
 def before_all(context):
-    working_dir = pathlib.Path(__file__).resolve().parent.parent.parent.parent
+    test_dir = pathlib.Path(__file__).resolve().parent.parent
+    test_dir.joinpath('cabot').unlink(missing_ok=True)
+    working_dir = test_dir.parent.parent
     os.chdir(working_dir)
     subprocess.run(['cargo', 'build', '--release'])
+    copy2(
+        working_dir.joinpath('target', 'release', 'cabot'),
+        test_dir,
+    )
+    os.chdir(test_dir)
+    os.environ['PATH'] = os.environ['PATH'] + ':.'
 
 
+def before_scenario(context, scenario):
+    context.stash = {}
+    context.run = run_command(context)
--- tests/functionals/functionals/help.py
+++ tests/functionals/functionals/help.py
@@ -2,11 +2,11 @@ from behave import *
 
 @when('I run "{command}"')
 def run_command(context, command):
-    context.stash['command'] = command
-    context.stash['result'] = ''
-
+    context.stash['result'] = context.run(command)
 
 @then('I can read the command options')
 def read_options(context):
-    pass
+    assert context.stash['result'].returncode == 0
+    stdout = context.stash['result'].stdout
+    assert 'cabot [FLAGS] [OPTIONS] <URL>' in stdout
 
